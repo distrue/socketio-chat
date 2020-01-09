18c7989e12565dda82f085985ad459e5a4667d81
unknown <junhyeog@korea.ac.kr>
2020-01-09 18:45:24 +0900

    채팅 상세 page 1차 구현

---
+++ components/mobileWrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  width: 100%;
+  max-width: 450px;
+  margin: 0 auto;
+  height: 100vh;
+  overflow: hidden;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0%);
+  top: 0;
+`;
